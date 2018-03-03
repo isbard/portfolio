94db194ff81c29160cca6d850c3445ff3defd456
isbard <isbard36@hotmail.com>
2018-03-03 17:44:50 +0000

    Mobile menu
    Starting with the mobile view, work on the axpanding menu done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-$(document).foundation()
+
+	//Cross the menu burger icon
+	function cross(x) {
+		x.classList.toggle('change');
+		$('.main-menu').slideToggle();
+		$('.main').toggleClass('dark');
+		$('#about-icons').slideToggle();
+	};
+	
+	
